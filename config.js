49444c6b9f011d08e12ec463e36c2e90a2168c80
MrDevyDev <158104074+MrDevyDev@users.noreply.github.com>
2024-02-04 15:09:54 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,93 @@
+module.exports = {
+    api: {
+        url: 'null',
+        path: 'null'
+    },
+    user: {
+        id: "1141156901665652949",
+        username: "rocky.121",
+        discriminator: "0",
+        nickname: "Rocky",
+        nickavatar: "https://media.discordapp.net/attachments/1203755858098520085/1203790596746252298/1544x1544_circle.png?ex=65d2605b&is=65bfeb5b&hm=811a84931696ea6281b5247d65f6ea1af5bfa6e24984588301b48541b0ef491b&=&format=webp&quality=lossless&width=662&height=662",
+        status: {
+            state: {
+                text: "dnd",
+                color: "#ED4245"
+            },
+            emote: "https://cdn.discordapp.com/emojis/682858108862726191.gif?size=2048",
+            text: "breaking stuff like usual"
+        },
+        activities: [],
+        createdTimestamp: 1615924806867,
+        avatar: "",
+        banner: "https://cdn.waya.one/r/sunset.gif",
+        accentColor: "#ce3760"
+    },
+    cards: [{
+        name: 'SOON',
+        icon: 'https://cdn.discordapp.com/avatars/857230367350063104/a54dbb364757f99076f36003ae68f77f.png?size=1024',
+        description: 'SOON.',
+        url: 'https://google.com',
+        position: `Founder, Developer`,
+        bot: 2
+    },
+    ],
+    header: {
+        icon: `https://media.discordapp.net/attachments/1203755858098520085/1203790596746252298/1544x1544_circle.png?ex=65d2605b&is=65bfeb5b&hm=811a84931696ea6281b5247d65f6ea1af5bfa6e24984588301b48541b0ef491b&=&format=webp&quality=lossless&width=662&height=662`,
+        name: `Rocky`,
+        //buttons: {
+          //  Discord: `/go/discord`,
+            //GitHub: `/go/github`,
+            //Twitter: `/go/twitter`
+        //}
+    },
+    metadata: {
+        title: `Rocky - Software Engineer and LGBTQ+.`,
+        description: `Heyia, I'm Rocky, born at 3rd of January and currently live in USA.\nI'm Male and my pronouns are He/They!`,
+        image: `https://avatars.githubusercontent.com/u/71079641?v=4`,
+        domainName: `google.com`,
+        color: `#f19bec`,
+        keywords: [
+            'Luna',
+            'Lunish',
+            'Lunish#8888',
+            'Lunish Twitter',
+            'Lunish Discord',
+            'Lunish GitHub',
+            'Lunish Onlyfans',
+            'waya bot',
+            'waya',
+            'waya.one',
+            'discord waya',
+            'Seemann',
+            'Luna Seemann',
+            'Flamex',
+            'Bendix',
+            'dbleu',
+            'dbleu staff',
+            'discord-botlist.eu',
+            'discord-botlist.eu staff',
+            'Tixte',
+            'Tixte staff',
+            'Dasu',
+            'Dasu staff',
+            'Pronouns',
+            'Pronouns bot',
+            'Pronouns discord',
+            'discord bot',
+            'discord bots',
+            'discord',
+            'botlist',
+            'dlist.gg',
+            'dlist.js',
+            'dlistjs',
+            'dlist',
+            'dlist api',
+            'discordlist.gg',
+            'discordlist.js',
+            'discordlistjs',
+            'discordlist',
+            'discordlist api'
+        ],
+    }
+}
